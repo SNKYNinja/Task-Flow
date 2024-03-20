d6cff9d4fffee74d194e17c00831ff166855bdb0
Aditya Chaudhary <96983394+SNKYNinja@users.noreply.github.com>
2024-03-20 19:17:28 +0530

    feat: Use remotePatterns as domains is depreciated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["localhost", "https://img.clerk.com", "img.clerk.com"],
+        // Depreciated
+        // domains: ["localhost", "https://img.clerk.com", "img.clerk.com"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "https://img.clerk.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "img.clerk.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
     },
 };
 
